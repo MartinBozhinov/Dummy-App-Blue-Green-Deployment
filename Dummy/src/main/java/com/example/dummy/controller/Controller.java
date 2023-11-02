31bf08cb3c33dd17bce25c963011848a83362ec3
MartinBozhinov <sharkytrapera@gmail.com>
2023-11-02 13:29:45 +0200

    dummy app

---
+++ Dummy/src/main/java/com/example/dummy/controller/Controller.java
@@ -0,0 +1,54 @@
+package com.example.dummy.controller;
+
+import com.example.dummy.entity.Employee;
+import com.example.dummy.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No id "));
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
